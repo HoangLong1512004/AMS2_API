bfeeb1f422f31a9db82977c890e326dd9426226b
longn <longnhph42829@fpt.edu.vn>
2024-04-08 23:14:04 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express();
+const port = 3000
+const mongoose = require('mongoose');
+
+const cayModel = require('./model/cayModel')
+
+const path = require('path');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+
+app.listen(port, () => {
+    console.log(`Server đang chạy ở cổng ${port}`)
+})
+
+const uri = 'mongodb+srv://hoanglong1512004:VuongChiLong@cluster0.puka1aw.mongodb.net/AMS'
+
+app.use(express.json());
+
+app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
+
+const api = require('./api');
+app.use('/api', api)
+
+try {
+    mongoose.connect(uri)
+    console.log('Ket noi thanh cong');
+} catch (error) {
+    console.log('Loi: ', error);
+}
+
+app.get('/', async (req, res) => {
+    let cay = await cayModel.find();
+
+    res.send(cay)
+})
